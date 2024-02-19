5c772385258f523cb828a17735478ace123e4df4
vanhobmt99 <57746121+vanhobmt99@users.noreply.github.com>
2024-02-19 21:38:58 +0700

    testtime

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ app.get('/', function (req, res) {
 app.get('/about', function (req, res) {
     res.send('About Us');
     });
+
+ app.get('/contact', function (req, res) {
+   //i want to show time in vietnam with gmt+7 although my server is in US
+    var date = new Date();
+    var vietnamTime = date.getTime() + (420*60*1000);
+    var vietnamDate = new Date(vietnamTime);
+    res.send('Vietnam Time: ' + vietnamDate);
+
+    });
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
