0d37b25bd8ff209d3c9e6931b9619fbe4a08fc07
vanhobmt99 <57746121+vanhobmt99@users.noreply.github.com>
2024-02-19 21:31:28 +0700

    test

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/about', function (req, res) {
+    res.send('About Us');
+    });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
