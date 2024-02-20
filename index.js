64af784a22e64de6d82e17a52fb25daca425f14a
vanhobmt99 <57746121+vanhobmt99@users.noreply.github.com>
2024-02-20 16:29:30 +0700

    update bot telegram

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '7182311087:AAHQImo76a2qKoJZmvSdi1evFhPv6A5JIm4';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+//get date time of the message in vietnam
+  var date = new Date();
+  date.setHours(date.getHours() + 7);
+  
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId,date);
+});
